2e76bc9355da190656a426b9303f65d4532319a6
Rahul-1611 <rahuldeshmukh1611@gmail.com>
2023-11-25 04:27:18 +0530

    emailValid done

--- src/components/Main/EmailValidator/EValid.jsx
+++ src/components/Main/EmailValidator/EValid.jsx
@@ -1,20 +1,51 @@
 import EmailValidation from "emailvalid";
+import { useState } from "react";
+import Result from "./Result";
 export default function EValid() {
-    const ev = new EmailValidation({ allowFreemail: true });
+    const ev = new EmailValidation();
+    const [ans, setAns] = useState({
+        email: null,
+        show: false,
+        valid: null,
+        freemail: null,
+        disposable: null,
+        typo: null
+    });
     const handleSubmit = (e) => {
         e.preventDefault();
         let email = e.target[0].value;
         const result = ev.check(email);
-        console.log(result);
+        let isValid = false;
+        if (result.valid) {
+            isValid = true;
+        } else if (result.errors.length == 1) {
+            if (result.errors.includes('freemail')) isValid = true;
+        }
+        setAns({
+            email: result.email,
+            show: true,
+            valid: isValid,
+            disposable: result.errors.includes('disposable'),
+            freemail: result.errors.includes('freemail'),
+            typo: result.typo !== null
+        })
     }
     return (
         <div className="w-3/5">
             <form onSubmit={handleSubmit}>
-                <div className="border border-orange-900 rounded-full bg-background min-w-[15rem] h-12 p-1 flex justify-between align-center text-primary ">
-                    <input type="email" name="email" placeholder="example@xyz.com" className="bg-transparent focus:outline-none autofill:bg-background w-full mx-3 leading-4 placeholder:text-muted-foreground" />
-                    <button type="submit" className="text-xs hover:text-sm p-0 rounded-full w-20 bg-orange-700">Submit</button>
+                <div className="border border-orange-900 rounded-full bg-background min-w-[15rem] h-12 p-1 flex justify-between items-center text-primary">
+                    <input maxLength='80' type="text" name="email" placeholder="example@xyz.com" className="bg-transparent focus:outline-none autofill:bg-background w-full mx-3 leading-4 placeholder:text-muted-foreground" />
+                    <button type="submit" className="text-xs hover:text-sm h-full rounded-full w-24 bg-orange-700">Submit</button>
                 </div>
             </form>
+            <div className={`duration-500 ${ans.show ? 'opacity-100' : 'opacity-0'}`}>
+                <Result
+                    email={ans.email}
+                    valid={ans.valid}
+                    freemail={ans.freemail}
+                    disposable={ans.disposable}
+                    typo={ans.typo} />
+            </div>
         </div>
     )
 }
---
+++ src/components/Main/EmailValidator/Result.jsx
@@ -0,0 +1,27 @@
+import { PiXCircle } from "react-icons/pi";
+import { LiaCheckCircle } from "react-icons/lia";
+export const Result = ({ email, valid, freemail, disposable, typo }) => {
+    return (
+        <div className="text-primary border-4 border-orange-700 rounded-2xl mt-5">
+            <p className="text-center py-3 border-b-4 border-orange-700"> {email} is a
+                {valid ? <span className="text-green-900 bg-green-500 px-2 py-1 rounded-md mx-1">Valid</span>
+                    : <span className="text-red-700 bg-red-300 px-2 py-1 rounded-md mx-1">Invalid</span>}
+                email.
+            </p>
+            <div className="grid grid-flow-col justify-stretch">
+                <div className="flex flex-col items-center py-3"><Icon input={freemail} />Freemail</div>
+                <div className="flex flex-col border-x-4 border-orange-700 items-center py-3"><Icon input={disposable} />Disposable</div>
+                <div className="flex flex-col items-center py-3"><Icon input={typo} />Typo</div>
+            </div>
+        </div>
+    )
+}
+const Icon = ({ input }) => {
+    return (
+        <>
+            {input ? <LiaCheckCircle size={80} className="text-green-600" /> :
+                <PiXCircle size={80} className="text-red-700" />}
+        </>
+    )
+}
+export default Result;
